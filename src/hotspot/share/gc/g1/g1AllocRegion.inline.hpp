bba6f571c11e2b137c34c5e7896be5573e9d18ff
Paul Lehner <paul@debian.local>
2026-02-20 14:46:39 -0600

    add potential survivor hole after retiring region

--- src/hotspot/share/gc/g1/g1AllocRegion.inline.hpp
+++ src/hotspot/share/gc/g1/g1AllocRegion.inline.hpp
@@ -83,7 +83,11 @@ inline HeapWord* G1AllocRegion::attempt_allocation_locked(size_t min_word_size,
 inline HeapWord* G1AllocRegion::attempt_allocation_using_new_region(size_t min_word_size,
                                                                     size_t desired_word_size,
                                                                     size_t* actual_word_size) {
-  retire(true /* fill_up */);
+  G1HeapRegion* alloc_region = _alloc_region;                                      
+  size_t waste = retire(true /* fill_up */);
+  if (alloc_region->is_survivor()) {
+    _g1h->_tracker.add_potential_survivor_hole(alloc_region, alloc_region->pre_dummy_top(), waste);
+  }
   HeapWord* result = new_alloc_region_and_allocate(desired_word_size);
   if (result != nullptr) {
     *actual_word_size = desired_word_size;
