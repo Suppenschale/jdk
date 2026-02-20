8d2dd0a372ed8adbae1eef2a0c72393703739185
Paul Lehner <paul@debian.local>
2026-02-20 15:00:50 -0600

    clean up holes after young gc

--- src/hotspot/share/gc/g1/g1YoungGCPostEvacuateTasks.cpp
+++ src/hotspot/share/gc/g1/g1YoungGCPostEvacuateTasks.cpp
@@ -334,6 +334,8 @@ G1PostEvacuateCollectionSetCleanupTask1::G1PostEvacuateCollectionSetCleanupTask1
   if (evac_failed) {
     add_parallel_task(new RestoreEvacFailureRegionsTask(evac_failure_regions));
   }
+
+  G1CollectedHeap::heap()->_tracker.clean_up_holes();
 }
 
 class G1FreeHumongousRegionClosure : public G1HeapRegionIndexClosure {
